dea4077140c28714ddd1745820d4f8616400d0ec
Juan Leonel <vazquez.juan.1fm@gmail.com>
2021-08-28 14:51:31 -0500

    PROYECTO PARA EXPO

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,74 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_article, except: [:new, :create, :index, :home_view]
+
+  # GET /articles or /articles.json
+  def index
+    @articles = current_user.articles.all
+  end
+
+  # GET /articles/1 or /articles/1.json
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = current_user.articles.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles or /articles.json
+  def create
+    @article = current_user.articles.create(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles/1 or /articles/1.json
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: "Article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles/1 or /articles/1.json
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  def home_view
+    @articles = Article.all
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:name, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :articles
+
+  root to: "home#index"
+  get "todo", to: "articles#home_view"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
